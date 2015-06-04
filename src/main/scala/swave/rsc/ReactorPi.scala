e4d4987564447c6689c32743f6cd832658c6fa62
Mathias <mathias@spray.io>
2015-06-04 22:01:59 +0200

    Simplify ReactorPi for easier deadlock analysis.

--- src/main/scala/swave/rsc/ReactorPi.scala
+++ src/main/scala/swave/rsc/ReactorPi.scala
@@ -1,15 +1,12 @@
 package swave.rsc
 
-import scala.io.StdIn
 import java.util.concurrent.Executors
-import java.util.{ List ⇒ JList }
 import reactor.Environment
-import reactor.core.processor.{ RingBufferWorkProcessor, RingBufferProcessor }
+import reactor.core.processor.RingBufferProcessor
 import reactor.core.support.NamedDaemonThreadFactory
 import reactor.rx._
+import scala.io.StdIn
 import scala.collection.JavaConverters._
-import Model._
-import Utils._
 
 object ReactorPi extends App {
   import ReactorCompat._
@@ -17,10 +14,6 @@ object ReactorPi extends App {
   Environment.initialize()
 
   val dispatcher1 = Environment.sharedDispatcher()
-  //  val dispatcher2 = Environment.cachedDispatcher()
-  //  val dispatcher3 = Environment.cachedDispatcher()
-  //  val dispatcher4 = Environment.cachedDispatcher()
-  //  val dispatcher5 = Environment.cachedDispatcher()
 
   val pool = Executors.newCachedThreadPool(new NamedDaemonThreadFactory("tee", null, null, true))
 
@@ -45,44 +38,6 @@ object ReactorPi extends App {
     .take(1000)
     .consume()
     .start()
-
-  //  val points: Stream[Point] =
-  //    Streams.from(iterable(new RandomDoubleValueGenerator()).asJava)
-  //      .dispatchOn(dispatcher1).subscribeOn(dispatcher1)
-  //      .buffer(2)
-  //      .map[Point] { (list: JList[Double]) ⇒ Point(list.get(0), list.get(1)) }
-  //      //.log("points")
-  //      .broadcast()
-  //
-  //  val innerSamples: Stream[Sample] =
-  //    points.dispatchOn(dispatcher2).subscribeOn(dispatcher2)
-  //      //.log("inner-1")
-  //      .filter { (p: Point) ⇒ p.isInner }
-  //      .map[Sample](InnerSample(_: Point))
-  //  //.broadcast()
-  //  //.log("inner-2")
-  //
-  //  val outerSamples: Stream[Sample] =
-  //    points.dispatchOn(dispatcher3).subscribeOn(dispatcher3)
-  //      //.log("outer-1")
-  //      .filter { (p: Point) ⇒ !p.isInner }
-  //      .map[Sample](OuterSample(_: Point))
-  //  //.broadcast()
-  //  //.log("outer-2")
-  //
-  //  //val simulationStates: Stream[SimulationState] =
-  //  Streams.merge(innerSamples, outerSamples)
-  //    //.dispatchOn(dispatcher4).subscribeOn(dispatcher4)
-  //    //.log("D")
-  //    .scan(SimulationState(0, 0), { (_: SimulationState).withNextSample(_: Sample) })
-  //
-  //    //simulationStates.dispatchOn(dispatcher5).subscribeOn(dispatcher5)
-  //    //.throttle(1000)
-  //    //.map[String] { (ss: SimulationState) ⇒ f"After ${ss.totalSamples}%8d samples π is approximated as ${ss.π}%.5f" }
-  //    .log("RESULT")
-  //    .take(10000)
-  //    .consume()
-  //    .start()
 
   StdIn.readLine()
   Environment.terminate()
